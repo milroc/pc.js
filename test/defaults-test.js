47871498c17bea4f44fab469303c7bae568fe818
Miles McCrocklin <miles.mccrocklin@gmail.com>
2012-07-31 23:27:07 -0700

    added defaults only test behavior.

--- test/defaults-test.js
+++ test/defaults-test.js
@@ -49,15 +49,39 @@ suite.addBatch({
 			assert.strictEqual(typeof f.bar(), "function");
 			assert.equal((f.bar())(), 1);
 			assert.equal(f(), 1);
-			assert.equal
 			f.bar(function() { return 4; });
 			assert.equal((f.bar())(), 4);
 			assert.equal(f(), 4);
 			f.bar("two");
 			assert.equal((f.bar())(), 2);
 			assert.equal(f(), 2);
-			
-		}
+		},
+		"test default only behavior": function(default_property) {
+			function foo() {
+				ret.bar = default_property("one", {
+					"one": function() { return 1; },
+					"two": function() { return 2; },
+					"three": function() { return 3; } 
+				}, true);
+				function ret() {
+					var bar = ret.bar();
+					return bar();
+				}
+				return ret;
+			}
+
+			var f = foo();
+
+			assert.strictEqual(typeof f.bar(), "function");
+			assert.equal((f.bar())(), 1);
+			assert.equal(f(), 1);
+			f.bar(function() { return 4; });
+			assert.equal((f.bar())(), 1);
+			assert.equal(f(), 1);
+			f.bar("two");
+			assert.equal((f.bar())(), 2);
+			assert.equal(f(), 2);
+		} 
 	}
 });
 
